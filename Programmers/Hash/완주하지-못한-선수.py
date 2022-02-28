f93f5a982d25e3ab1ae877b2416f6e9422a125d4
HyoungSeok Kim <59719632+OcuIus@users.noreply.github.com>
2022-03-01 00:24:52 +0900

    Add files via upload

---
+++ Programmers/Hash/완주하지-못한-선수.py
@@ -0,0 +1,8 @@
+from collections import Counter
+def solution(participant, completion):
+    p=Counter(participant)
+    c=Counter(completion)
+    for i in p.keys():
+        if c[i]==False or p[i] != c[i]:
+            answer=i
+    return answer
